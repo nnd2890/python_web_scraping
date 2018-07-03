3356151a08e6fa826d8038de943aae0552af1bca
nnd2890 <dainnd00258@fpt.edu.vn>
2018-07-03 19:45:03 +0700

    find children

---
+++ First Web Scraper/scrape_shop_children.py
@@ -0,0 +1,7 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen('http://pythonscraping.com/pages/page3.html')
+bsObj = BeautifulSoup(html,'lxml')
+for child in bsObj.find('table',{'id':'giftList'}).descendants:
+    print(child)
